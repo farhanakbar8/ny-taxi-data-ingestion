0edf10bcbd463f4e305263bad2f2dad9713cb772
Farhan <farhanakbarr008@gmail.com>
2023-02-12 10:20:52 +0700

    refactor ingest.py

--- ingest.py
+++ ingest.py
@@ -1,38 +1,43 @@
-### Import libraries
 import pandas as pd
 from sqlalchemy import create_engine
 from time import time
 
-### Read 100 examples of NY Taxi Data
-data = pd.read_csv('yellow_tripdata_2022-01.csv', nrows=100)
-data.drop(columns='Unnamed: 0', inplace=True)
-print("**100 examples of NY Taxi Data**")
-print(data.head())
+def load_data_iterator():
+    data_iter = pd.read_csv('yellow_tripdata_2022-01.csv', iterator=True, chunksize=100000)
+    return data_iter
 
-### Connect to Database
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-engine.connect()
-
-print("**CREATE TABLE query**")
-print(pd.io.sql.get_schema(data, 'yellow_taxi_data', con=engine))
-
-### Create empty table
-data.head(0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-### Create dataframe iterator to ingest the data to database
-df_iter = pd.read_csv('yellow_tripdata_2022-01.csv', iterator=True, chunksize=100000)
-
-### Append 100000 data at a time
-while True:
-    t_start = time()
-
-    data = next(df_iter)
-    data = data.drop(columns='Unnamed: 0')
+def load_data_chunk(data):
+    data.drop(columns='Unnamed: 0', inplace=True)
     data.tpep_dropoff_datetime = pd.to_datetime(data.tpep_dropoff_datetime)
     data.tpep_pickup_datetime = pd.to_datetime(data.tpep_pickup_datetime)
+    return data
 
+def create_empty_table(data_iter, engine):
+    data = next(data_iter)
+    data.drop(columns='Unnamed: 0', axis=0, inplace=True)
+    data.head(0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+    ingest_to_db(data, engine)
+
+def ingest_to_db(data, engine):
     data.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
 
-    t_end = time()
+def main():
+    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    engine.connect()
+    data_iter = load_data_iterator()
 
-    print(f'inserted a batch of data, took {t_end - t_start} seconds')
+    create_empty_table(data_iter, engine)
+
+    for data in data_iter:
+        t_start = time()
+        
+        data = load_data_chunk(data)
+        ingest_to_db(data, engine)
+
+        t_end = time()
+
+        print(f'inserted a batch of data, took {t_end - t_start} seconds')
+
+
+if __name__ == "__main__":
+    main()
